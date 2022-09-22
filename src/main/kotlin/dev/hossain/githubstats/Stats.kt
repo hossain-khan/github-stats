ea39b8f405d156bea6ea7e14ce43416913e5beff
Hossain Khan <hello@hossain.dev>
2022-09-22 12:10:01 -0400

    [IGNORE] Local props with keys
    Also added test code

--- src/main/kotlin/dev/hossain/githubstats/Stats.kt
+++ src/main/kotlin/dev/hossain/githubstats/Stats.kt
@@ -1,7 +1,19 @@
 package dev.hossain.githubstats
 
+import java.io.File
+import java.util.*
+
 fun main(args: Array<String>) {
     println("Hello World!")
+
+    val propertiesFile = File("local.properties")
+    if (propertiesFile.exists()) {
+        val properties = Properties()
+        properties.load(propertiesFile.inputStream())
+        println(properties.containsKey("token"))
+    } else {
+        println("No properties file")
+    }
 
     println("Program arguments: ${args.joinToString()}")
 }
