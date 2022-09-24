9812196af432c33d8cb48e5a600ac7c8f084325c
Hossain Khan <hello@hossain.dev>
2022-09-24 09:08:57 -0400

    [ADDED] Table based report
    Prints:
    
    ```
    ┌──────────────┬──────────────────────────────────────────────────────────┐
    │ Title        │ fix(curriculum): make notation of class names consistent │
    ├──────────────┼──────────────────────────────────────────────────────────┤
    │ URL          │ https://github.com/freeCodeCamp/freeCodeCamp/pull/47594  │
    ├──────────────┼──────────────────────────────────────────────────────────┤
    │ Ready On     │ Sep 22, 2022, 12:10:46 PM                                │
    ├──────────────┼──────────────────────────────────────────────────────────┤
    │ Review Time  │ naomi-lgbt=7h 54m 29s                                    │
    │              ├──────────────────────────────────────────────────────────┤
    │              │ Sboonny=21h 3m 39s                                       │
    ├──────────────┼──────────────────────────────────────────────────────────┤
    │ Merged On    │ Sep 23, 2022, 4:28:08 PM                                 │
    ├──────────────┼──────────────────────────────────────────────────────────┤
    │ Open → Merge │ 1d 4h 17m 22s                                            │
    └──────────────┴──────────────────────────────────────────────────────────┘
    ```
    
    Closes #20

--- src/main/kotlin/dev/hossain/githubstats/Stats.kt
+++ src/main/kotlin/dev/hossain/githubstats/Stats.kt
@@ -1,21 +1,15 @@
 package dev.hossain.githubstats
 
+import dev.hossain.githubstats.formatter.PicnicTableFormatter
+import dev.hossain.githubstats.formatter.StatsFormatter
 import dev.hossain.githubstats.io.Client
 import kotlinx.coroutines.runBlocking
-import kotlinx.datetime.toJavaInstant
-import java.time.ZoneId
-import java.time.format.DateTimeFormatter
-import java.time.format.FormatStyle
-import java.util.Locale
 
 fun main(args: Array<String>) {
     println("Program arguments: ${args.joinToString()}")
 
-    val dateFormatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)
-        .withLocale(Locale.US)
-        .withZone(ZoneId.systemDefault())
-
     val pullStats = PullStats(Client.githubService)
+    val formatter: StatsFormatter = PicnicTableFormatter()
     runBlocking {
         // Interesting PRs:
         // https://github.com/square/retrofit/pull/3613
@@ -25,18 +19,8 @@ fun main(args: Array<String>) {
             is PullStats.StatsResult.Failure -> {
                 println("Got error for stats: ${result.error}")
             }
-
             is PullStats.StatsResult.Success -> {
-                println("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
-                println("PR: ${result.pullRequest.title} (${result.pullRequest.html_url})")
-                println("PR Available: ${dateFormatter.format(result.prReadyOn.toJavaInstant())}")
-                println("Review Time: ${result.reviewTime}")
-                println(
-                    "PR Merged in: ${result.prMergedOn - result.prReadyOn} on ${
-                    dateFormatter.format(result.prMergedOn.toJavaInstant())
-                    }"
-                )
-                println("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
+                println(formatter.formatPrStats(result))
             }
         }
     }
