64d2e4f8a00252d84d512ff774f2d06af452ab05
Hossain Khan <hello@hossain.dev>
2022-09-27 11:22:47 -0400

    [ADDED] Issue search paging support
    Fixes #64

--- src/main/kotlin/dev/hossain/githubstats/model/IssueSearchResult.kt
+++ src/main/kotlin/dev/hossain/githubstats/model/IssueSearchResult.kt
@@ -1,5 +1,10 @@
 package dev.hossain.githubstats.model
 
+/**
+ * Model class for GitHub issue search result.
+ *
+ * https://docs.github.com/en/rest/search#search-issues-and-pull-requests
+ */
 data class IssueSearchResult(
     val total_count: Int,
     val items: List<Issue>
