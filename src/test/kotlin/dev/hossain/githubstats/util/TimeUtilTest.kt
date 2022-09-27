020995337f692527fd778a89ab9414090f229014
Hossain Khan <hello@hossain.dev>
2022-09-26 23:37:23 -0400

    [ADDED] Test code to check viability
    https://github.com/hossain-khan/github-stats/issues/67 Ref links added theere

--- src/test/kotlin/dev/hossain/githubstats/util/TimeUtilTest.kt
+++ src/test/kotlin/dev/hossain/githubstats/util/TimeUtilTest.kt
@@ -2,6 +2,9 @@ package dev.hossain.githubstats.util
 
 import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.periodUntil
+import kotlinx.datetime.toDateTimePeriod
 import kotlinx.datetime.toInstant
 import kotlinx.datetime.toJavaInstant
 import org.junit.jupiter.api.Test
@@ -48,5 +51,37 @@ internal class TimeUtilTest {
         println(shortFormat.format(instantNow.toJavaInstant()))
         println(mediumFormat.format(instantNow.toJavaInstant()))
         println(fullFormat.format(instantNow.toJavaInstant()))
+    }
+
+    @Test
+    fun `given two time - the difference accounts for working hours`() {
+        println("- - - - - - - - - - - - - - - - - - - - - - - - -")
+        // For example, working hours are usually from 9:00AM-5:00PM
+        // Date#1: January 1, 2022 10:00AM
+        // Date#2: January 3, 2020 02:00PM
+        // The difference should be 20 hours
+
+        val mediumFormat = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.FULL)
+            .withLocale(Locale.US)
+            .withZone(ZoneId.systemDefault())
+
+        // Sat Jan 01 2022 10:00:00 GMT-0500 (Eastern Standard Time)
+        // Sat Jan 01 2022 15:00:00 GMT+0000
+        val date1: Instant = Instant.fromEpochSeconds(1641049200)
+        println(mediumFormat.format(date1.toJavaInstant()))
+
+        // Mon Jan 03 2022 14:00:00 GMT-0500 (Eastern Standard Time)
+        // Mon Jan 03 2022 19:00:00 GMT+0000
+        val date2: Instant = Instant.fromEpochSeconds(1641236400)
+        println(mediumFormat.format(date2.toJavaInstant()))
+
+        val dateTimePeriod = date1.periodUntil(date2, TimeZone.currentSystemDefault())
+        println("dateTimePeriod=$dateTimePeriod")
+
+        val duration: Duration = date2 - date1
+        println("Difference duration = $duration")
+        println("Difference duration in period = ${duration.toDateTimePeriod()}")
+
+        println("- - - - - - - - - - - - - - - - - - - - - - - - -")
     }
 }
