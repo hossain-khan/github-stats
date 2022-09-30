14f33879e8c8785fc337a00af91d23f86e19ca08
Hossain Khan <hello@hossain.dev>
2022-09-30 00:21:22 -0400

    [ADDED] #67 More test code to detect next non working hour.

--- src/main/kotlin/time/DateTimeDiffer.kt
+++ src/main/kotlin/time/DateTimeDiffer.kt
@@ -2,6 +2,7 @@ package time
 
 import kotlinx.datetime.Instant
 import kotlinx.datetime.toJavaInstant
+import org.threeten.extra.Temporals
 import java.time.ZoneId
 import java.time.ZonedDateTime
 import kotlin.time.Duration
@@ -11,7 +12,31 @@ object DateTimeDiffer {
         val startDateTime: ZonedDateTime = startTime.toJavaInstant().atZone(zoneId)
         val endDateTime: ZonedDateTime = endTime.toJavaInstant().atZone(zoneId)
 
+        /*
+         * Things to consider:
+         * - Start DateTime:
+         *   • Was it opened during working day and working hour?
+         *   • When is the next working day and hour and the difference between start date time?
+         *   • Need to ensure that next working day+hour does not exceed the end time
+         * - End DateTime
+         *   • Was it done during working day and time?
+         *   • What if it was done in non-working hour (after hours)? how do we credit the reviewer?
+         */
+
         println("startDateTime=$startDateTime and $endDateTime")
+
+        val startTimeNextWorkingDay = startDateTime.with(Temporals.nextWorkingDayOrSame())
+        val endTimeNextWorkingDay = endDateTime.with(Temporals.nextWorkingDayOrSame())
+        println("startTimeNextWorkingDay=$startTimeNextWorkingDay : " + startTimeNextWorkingDay.equals(startDateTime))
+        println("endTimeNextWorkingDay=$endTimeNextWorkingDay : " + endTimeNextWorkingDay.equals(endDateTime))
+
+        if (startTimeNextWorkingDay.equals(startDateTime).not()) {
+            val nextWorkingDayDiff = java.time.Duration.between(startDateTime, startTimeNextWorkingDay)
+            println("nextWorkingDayDiff=$nextWorkingDayDiff")
+        }
+
+        val nonWorkingHour = startDateTime.with(TemporalsExtension.nextNonWorkingHourOrSame())
+        println("Next nonWorkingHour = $nonWorkingHour")
 
         return endTime - startTime
     }
--- src/test/kotlin/time/DateTimeDifferTest.kt
+++ src/test/kotlin/time/DateTimeDifferTest.kt
@@ -17,7 +17,7 @@ internal class DateTimeDifferTest {
     }
 
     @Test
-    fun diffWorkingHours() {
+    fun `diffWorkingHours - given start time is weekends`() {
         // Sat Jan 01 2022 10:00:00 GMT-0500 (Eastern Standard Time)
         // Sat Jan 01 2022 15:00:00 GMT+0000
         val startTime: Instant = Instant.fromEpochSeconds(1641049200)
@@ -29,6 +29,41 @@ internal class DateTimeDifferTest {
         // America/Toronto
         val zoneId = ZoneId.systemDefault()
 
-        DateTimeDiffer.diffWorkingHours(startTime, endTime, zoneId)
+        val diffWorkingHours = DateTimeDiffer.diffWorkingHours(startTime, endTime, zoneId)
+        println("RESULT: diffWorkingHours() = $diffWorkingHours")
+    }
+
+    @Test
+    fun `diffWorkingHours - given start time is before work hour beings`() {
+        // Fri Jan 07 2022 03:00:00 GMT-0500 (Eastern Standard Time)
+        // Fri Jan 07 2022 08:00:00 GMT+0000
+        val startTime: Instant = Instant.fromEpochSeconds(1641542400)
+
+        // Fri Jan 07 2022 15:00:00 GMT-0500 (Eastern Standard Time)
+        // Fri Jan 07 2022 20:00:00 GMT+0000
+        val endTime: Instant = Instant.fromEpochSeconds(1641585600)
+
+        // America/Toronto
+        val zoneId = ZoneId.systemDefault()
+
+        val diffWorkingHours = DateTimeDiffer.diffWorkingHours(startTime, endTime, zoneId)
+        println("RESULT: diffWorkingHours() = $diffWorkingHours")
+    }
+
+    @Test
+    fun `diffWorkingHours - given start time is after work hour ends`() {
+        // Wed Jan 05 2022 21:00:00 GMT-0500 (Eastern Standard Time)
+        // Thu Jan 06 2022 02:00:00 GMT+0000
+        val startTime: Instant = Instant.fromEpochSeconds(1641434400)
+
+        // Thu Jan 06 2022 14:00:00 GMT-0500 (Eastern Standard Time)
+        // Thu Jan 06 2022 19:00:00 GMT+0000
+        val endTime: Instant = Instant.fromEpochSeconds(1641495600)
+
+        // America/Toronto
+        val zoneId = ZoneId.systemDefault()
+
+        val diffWorkingHours = DateTimeDiffer.diffWorkingHours(startTime, endTime, zoneId)
+        println("RESULT: diffWorkingHours() = $diffWorkingHours")
     }
 }
