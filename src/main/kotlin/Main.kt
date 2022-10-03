145cb1c96a5bf067be5b99d295b1c6d642e75386
Hossain Khan <hello@hossain.dev>
2022-10-03 14:21:53 -0400

    [REFACTOR] Moved `Stats.kt` to `Main.kt`

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,43 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import dev.hossain.githubstats.BuildConfig
+import dev.hossain.githubstats.PrAuthorStats
+import dev.hossain.githubstats.PullStats
+import dev.hossain.githubstats.formatter.CsvFormatter
+import dev.hossain.githubstats.formatter.FileWriterFormatter
+import dev.hossain.githubstats.formatter.PicnicTableFormatter
+import dev.hossain.githubstats.formatter.StatsFormatter
+import dev.hossain.githubstats.io.Client.githubService
+import dev.hossain.githubstats.service.IssueSearchPager
+import dev.hossain.githubstats.util.LocalProperties
+import kotlinx.coroutines.runBlocking
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+/**
+ * Runs PR stats on specified repository for specific autor.
+ * See **`local_sample.properties`** for more information on configuration available.
+ * Also check out [BuildConfig] for available runtime config for debugging.
+ */
+fun main(args: Array<String>) {
     println("Program arguments: ${args.joinToString()}")
+
+    val issueSearchPager = IssueSearchPager(githubService)
+    val pullStats = PullStats(githubService)
+    val authorStats = PrAuthorStats(issueSearchPager, pullStats)
+    val formatters: List<StatsFormatter> = listOf(
+        PicnicTableFormatter(),
+        CsvFormatter(),
+        FileWriterFormatter(PicnicTableFormatter())
+    )
+    val localProperties = LocalProperties()
+    val repoOwner: String = localProperties.getRepoOwner()
+    val repoId: String = localProperties.getRepoId()
+    val prAuthorUserId = "naomi-lgbt" // "naomi-lgbt", "ieahleen"
+
+    println("Getting PR stats for author '$prAuthorUserId' from '$repoId' repository.")
+
+    runBlocking {
+        val prAuthorStats = authorStats.authorStats(repoOwner, repoId, prAuthorUserId)
+
+        formatters.forEach {
+            println(it.formatAuthorStats(prAuthorStats))
+        }
+    }
 }
