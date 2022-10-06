95941964b3f6b562ab01f55bc81464ed74b9acd1
Hossain Khan <hello@hossain.dev>
2022-10-05 22:16:56 -0400

    [CLEANUP] Use repo interface and presentation update

--- src/main/kotlin/dev/hossain/githubstats/PrAuthorStats.kt
+++ src/main/kotlin/dev/hossain/githubstats/PrAuthorStats.kt
@@ -1,8 +1,8 @@
 package dev.hossain.githubstats
 
 import dev.hossain.githubstats.model.Issue
+import dev.hossain.githubstats.repository.PullRequestStatsRepo
 import dev.hossain.githubstats.repository.PullRequestStatsRepo.StatsResult
-import dev.hossain.githubstats.repository.PullRequestStatsRepoImpl
 import dev.hossain.githubstats.service.IssueSearchPager
 import dev.hossain.githubstats.service.SearchParams
 import kotlinx.coroutines.delay
@@ -16,7 +16,7 @@ import kotlin.time.Duration
  */
 class PrAuthorStats constructor(
     private val issueSearchPager: IssueSearchPager,
-    private val pullRequestStatsRepo: PullRequestStatsRepoImpl
+    private val pullRequestStatsRepo: PullRequestStatsRepo
 ) {
 
     /**
