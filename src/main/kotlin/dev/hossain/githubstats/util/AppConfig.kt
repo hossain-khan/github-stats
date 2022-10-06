d630e008c736482726c5a9504c94819bd22a35c3
Hossain Khan <hello@hossain.dev>
2022-10-06 17:45:38 -0400

    [ADDED] Date validator. I missed it recently.

--- src/main/kotlin/dev/hossain/githubstats/util/AppConfig.kt
+++ src/main/kotlin/dev/hossain/githubstats/util/AppConfig.kt
@@ -1,12 +1,36 @@
 package dev.hossain.githubstats.util
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
+import java.time.format.ResolverStyle
+import java.util.Locale
+
 class AppConfig constructor(localProperties: LocalProperties) {
     private val repoOwner: String = localProperties.getRepoOwner()
     private val repoId: String = localProperties.getRepoId()
-    private val dateLimit: String = localProperties.getDateLimit()
+    private val dateLimit: String = requireValidDate(localProperties.getDateLimit())
     private val prAuthorUserIds = localProperties.getAuthors().split(",")
         .filter { it.isNotEmpty() }
         .map { it.trim() }
 
     fun get(): Config = Config(repoOwner, repoId, dateLimit, prAuthorUserIds)
+
+    private fun requireValidDate(dateStr: String): String {
+        val dateFormatter: DateTimeFormatter = DateTimeFormatter
+            .ofPattern("uuuu-MM-dd", Locale.US)
+            .withResolverStyle(ResolverStyle.STRICT)
+        try {
+            dateFormatter.parse(dateStr)
+        } catch (e: DateTimeParseException) {
+            throw IllegalArgumentException(
+                "The date '$dateStr' should be formatted like `YYYY-MM-DD`. Today is `${
+                dateFormatter.format(
+                    LocalDate.now()
+                )
+                }`."
+            )
+        }
+        return dateStr
+    }
 }
