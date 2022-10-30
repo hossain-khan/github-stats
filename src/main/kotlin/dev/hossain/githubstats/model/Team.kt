6b331ba98e9210a3a8569252b660c32eb2228bf8
Hossain Khan <hello@hossain.dev>
2022-10-29 22:58:19 -0400

    [ADDED] Team review request parsing.

---
+++ src/main/kotlin/dev/hossain/githubstats/model/Team.kt
@@ -0,0 +1,47 @@
+package dev.hossain.githubstats.model
+
+/**
+ * Team for PR review.
+ * Groups of organization members that gives permissions on specified repositories.
+ *
+ * Example JSON:
+ * ```json
+ * {
+ *   "name": "opensearch-core",
+ *   "id": 4615246,
+ *   "node_id": "MDQ6VGVhbTQ2MTUyNDY=",
+ *   "slug": "opensearch-core",
+ *   "description": "Maintainers for OpenSearch core (OpenSearch engine) ",
+ *   "privacy": "closed",
+ *   "url": "https://api.github.com/organizations/80134844/team/4615246",
+ *   "html_url": "https://github.com/orgs/opensearch-project/teams/opensearch-core",
+ *   "members_url": "https://api.github.com/organizations/80134844/team/4615246/members{/member}",
+ *   "repositories_url": "https://api.github.com/organizations/80134844/team/4615246/repos",
+ *   "permission": "maintain",
+ *   "permissions": {
+ *     "admin": false,
+ *     "maintain": true,
+ *     "push": true,
+ *     "triage": true,
+ *     "pull": true
+ *   },
+ *   "parent": null
+ * }
+ * ```
+ */
+data class Team(
+    val id: Long,
+    /**
+     * Name of the team
+     */
+    val name: String,
+    val slug: String,
+    /**
+     * Description of the team
+     */
+    val description: String,
+    /**
+     * Permission that the team will have for its repositories
+     */
+    val permission: String
+)
