9a8a422a1b280e1797c4ace32f4199cd08d94c77
Hossain Khan <hello@hossain.dev>
2022-11-06 10:24:13 -0500

    [ADDED] Title and stats

--- src/main/kotlin/dev/hossain/githubstats/formatter/HtmlChartFormatter.kt
+++ src/main/kotlin/dev/hossain/githubstats/formatter/HtmlChartFormatter.kt
@@ -5,35 +5,45 @@ import dev.hossain.githubstats.AuthorReviewStats
 import dev.hossain.githubstats.PrStats
 import dev.hossain.githubstats.ReviewerReviewStats
 import dev.hossain.githubstats.formatter.html.Template
+import dev.hossain.githubstats.util.AppConfig
 import dev.hossain.githubstats.util.FileUtil
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 import java.io.File
 
 /**
  * Generates HTML based charts for the available data.
  * Currently, it uses [Google Chart](https://developers.google.com/chart) to generate simple charts.
  */
-class HtmlChartFormatter: StatsFormatter {
+class HtmlChartFormatter: StatsFormatter, KoinComponent {
+    private val appConfig: AppConfig by inject()
+
     override fun formatSinglePrStats(prStats: PrStats): String {
-        val formattedPrStats = ""
+        val formattedChart = ""
 
         val prStatsFileName = FileUtil.prReportChart(prStats)
-        File(prStatsFileName).writeText(formattedPrStats)
+        File(prStatsFileName).writeText(formattedChart)
 
         return ""
     }
 
     override fun formatAuthorStats(stats: List<AuthorReviewStats>): String {
         if (stats.isEmpty()) {
-            return "⚠ ERROR: No author stats to format. No files to write! ${Art.shrug}"
+            return "⚠ ERROR: No author stats to format. No charts to generate! ${Art.shrug}"
         }
 
         val prAuthorId = stats.first().prAuthorId
 
+        // Prepares data for chart generation
         val statsJsData = stats.map {
-            "['${it.reviewerId}', ${it.stats.size}]"
+            "['${it.reviewerId} [${it.stats.size}]', ${it.stats.size}]"
         }.joinToString()
 
-        val formattedChart = Template.pieChart(prAuthorId, statsJsData)
+        val formattedChart = Template.pieChart(
+            title = "PR reviewer`s stats for PRs created by `$prAuthorId` on `${appConfig.get().repoId}` repository " +
+                    "between ${appConfig.get().dateLimitAfter} and ${appConfig.get().dateLimitBefore}.",
+            statsJsData = statsJsData
+        )
 
         val combinedReportFileName = FileUtil.authorChartFile(prAuthorId)
         File(combinedReportFileName).writeText(formattedChart)
@@ -43,12 +53,12 @@ class HtmlChartFormatter: StatsFormatter {
 
     override fun formatReviewerStats(stats: ReviewerReviewStats): String {
         if (stats.reviewedPrStats.isEmpty() || stats.reviewedForPrStats.isEmpty()) {
-            return "⚠ ERROR: No reviewer stats to format. No files to write! ${Art.shrug}"
+            return "⚠ ERROR: No reviewer stats to format. No charts to generate! ${Art.shrug}"
         }
-        val formattedStats = ""
+        val formattedChart = ""
 
         val combinedReportFileName = FileUtil.reviewerReportFile(stats.reviewerId)
-        File(combinedReportFileName).writeText(formattedStats)
+        File(combinedReportFileName).writeText(formattedChart)
 
         return ""
     }
