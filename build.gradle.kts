4f916d6b4d23d937c6d1ca61dadaa1f07c997314
Hossain Khan <hello@hossain.dev>
2023-11-21 10:44:40 -0500

    [UPDATE] Gradle library dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     // For build.gradle.kts (Kotlin DSL)
     // https://kotlinlang.org/docs/releases.html#release-details
-    kotlin("jvm") version "1.7.10"
+    kotlin("jvm") version "1.9.20"
     id("org.jmailen.kotlinter") version "3.12.0"
     // https://kotlinlang.org/docs/ksp-quickstart.html#use-your-own-processor-in-a-project
-    // id("com.google.devtools.ksp") version "1.7.10-1.0.6" // Not needed yet.
+    // id("com.google.devtools.ksp") version "1.9.20-1.0.6" // Not needed yet.
     application
 }
 
@@ -19,7 +19,7 @@ repositories {
 
 dependencies {
     // https://github.com/Kotlin/kotlinx.coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
 
     // https://github.com/Kotlin/kotlinx-datetime
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
@@ -40,16 +40,16 @@ dependencies {
     implementation("com.jakewharton.picnic:picnic:0.6.0")
 
     // https://github.com/square/moshi
-    implementation("com.squareup.moshi:moshi:1.14.0")
+    implementation("com.squareup.moshi:moshi:1.15.0")
     implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
     implementation("com.squareup.moshi:moshi-adapters:1.14.0")
     // ksp("com.squareup.moshi:moshi-kotlin-codegen:1.14.0") // Not needed yet.
 
     // https://github.com/doyaaaaaken/kotlin-csv
-    implementation("com.github.doyaaaaaken:kotlin-csv-jvm:1.6.0") //for JVM platform
+    implementation("com.github.doyaaaaaken:kotlin-csv-jvm:1.8.0") //for JVM platform
 
     // Koin Core features - https://insert-koin.io/
-    implementation("io.insert-koin:koin-core:3.2.2")
+    implementation("io.insert-koin:koin-core:3.3.3")
 
     // ASCII Progress Bar https://github.com/ctongfei/progressbar
     implementation("me.tongfei:progressbar:0.9.5")
@@ -62,9 +62,9 @@ dependencies {
     testImplementation(kotlin("test"))
     testImplementation("com.google.truth:truth:1.1.3")
     testImplementation("com.squareup.okhttp3:mockwebserver:4.10.0")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:1.7.10")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:1.9.20")
 }
 
 tasks.test {
