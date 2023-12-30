7eccf83e75e2fa6958c3094c54ee4b09857402e8
Hossain Khan <hello@hossain.dev>
2023-12-29 21:20:51 -0500

    [REFACTOR] Moved city names into constants

---
+++ src/main/kotlin/dev/hossain/time/UserCity.kt
@@ -0,0 +1,15 @@
+package dev.hossain.time
+
+/**
+ * User cities for time zone defined in [UserTimeZone].
+ */
+object UserCity {
+    const val ATLANTA = "Atlanta"
+    const val CHICAGO = "Chicago"
+    const val DETROIT = "Detroit"
+    const val NEW_YORK = "New York"
+    const val PHOENIX = "Phoenix"
+    const val SAN_FRANCISCO = "San Francisco"
+    const val TORONTO = "Toronto"
+    const val VANCOUVER = "Vancouver"
+}
